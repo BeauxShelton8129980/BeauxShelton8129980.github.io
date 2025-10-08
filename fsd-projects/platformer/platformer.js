0b0a3ab379077de0260ac749fc6c8ae9d425e24a
BeauxShelton8129980 <bps8129980@lpssonline.com>
2025-10-08 14:30:43 +0000

    portfolio

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -32,7 +32,7 @@ $(function () {
 
     // TODO 2 - Create Platforms
 console.log(createPlatform(400, 600, 5, 10))
-console.log(createPlatform(600, 500, 10, 5)
+console.log(createPlatform(600, 500, 25, 15))
 
 
 
